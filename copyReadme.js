559494b3f28135138595ac13b7424246e4ffd0b3
mihinov <kamahinmihail@gmail.com>
2023-03-28 23:48:01 +0300

    docs: deploy readme

---
+++ copyReadme.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceFile = path.join(__dirname, 'README.md');
+const targetDir = path.join(__dirname, 'dist');
+
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir);
+}
+
+const targetFile = path.join(targetDir, 'README.md');
+fs.copyFileSync(sourceFile, targetFile);
+
+console.log('README.md файл успешно скопирован в папку dist');
