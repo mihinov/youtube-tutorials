432bf55fa8ef26c87c2b19e5f4e3a56b57f458d3
mihinov <kamahinmihail@gmail.com>
2023-03-30 20:40:40 +0000

    Deploying to gh-pages from @ mihinov/youtube-tutorials@ceca5dfaf14f62402978c9148a364d229445896e 🚀

--- youtube-security-supplies/index.b5bf94d8.js
+++
@@ -1,2 +0,0 @@
-const leftNode=document.querySelector(".intro__left"),rightNode=document.querySelector(".intro__right-img"),containerNode=document.querySelector(".intro .container");setInterval((()=>{console.log(getComputedStyle(rightNode).getPropertyValue("right"),"rightNode")}),500);
-//# sourceMappingURL=index.b5bf94d8.js.map
