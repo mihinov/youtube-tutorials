8833506e3955f2b9717b17823303c159417b8339
mihinov <kamahinmihail@gmail.com>
2023-04-14 16:11:30 +0300

    docs: pointer-events

--- copyReadme.js
+++ copyReadme.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const sourceFile = path.join(__dirname, 'README.md');
 const targetDir = path.join(__dirname, 'dist');
