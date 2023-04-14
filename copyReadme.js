f1eae6e75547bdbcf8d9d3cc31a27317bc7b8f57
mihinov <kamahinmihail@gmail.com>
2023-04-14 16:22:28 +0300

    docs: pointer-events

--- copyReadme.js
+++ copyReadme.js
@@ -1,5 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const sourceFile = path.join(__dirname, 'README.md');
 const targetDir = path.join(__dirname, 'dist');
