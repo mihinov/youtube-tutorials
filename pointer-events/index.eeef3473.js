5c4ce7789400a17112d7701ad8fd1fe3b8222e0b
mihinov <kamahinmihail@gmail.com>
2023-08-05 23:53:04 +0000

    Deploying to gh-pages from @ mihinov/youtube-tutorials@2116dedc74b944a4df26e311acbcf30af1e4a205 🚀

---
+++ pointer-events/index.eeef3473.js
@@ -0,0 +1,2 @@
+canvasNode.addEventListener("pointerdown",dragStart),canvasNode.addEventListener("pointerup",dragStop),canvasNode.addEventListener("pointermove",dragMove);
+//# sourceMappingURL=index.eeef3473.js.map
