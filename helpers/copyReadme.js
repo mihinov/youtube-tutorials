d2cd745dcdba6d591759d82fd5747269a71390bd
mihinov <kamahinmihail@gmail.com>
2023-12-20 15:41:00 +0300

    1

--- helpers/copyReadme.js
+++ helpers/copyReadme.js
@@ -12,7 +12,7 @@ if (!fs.existsSync(targetDir)) {
   fs.mkdirSync(targetDir);
 }
 
-const targetFile = path.join(targetDir, '../README.md');
+const targetFile = path.join(targetDir, 'README.md');
 fs.copyFileSync(sourceFile, targetFile);
 
 console.log('README.md файл успешно скопирован в папку dist');
