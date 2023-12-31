f93b73136fb23d3d60fbc3fb6c25b7716fd95bc9
mihinov <kamahinmihail@gmail.com>
2023-12-31 20:13:05 +0300

    .

--- ng-test-notes/src/app/modules/header/header.module.ts
+++ ng-test-notes/src/app/modules/header/header.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
-
-
 @NgModule({
   declarations: [
     HeaderComponent
