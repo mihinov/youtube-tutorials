b95acfb94875bea4283ed799bbae8a2f96f23645
mihinov <kamahinmihail@gmail.com>
2024-12-07 18:20:13 +0300

    1

--- youtube-css-paint-api/ts/index.ts
+++ youtube-css-paint-api/ts/index.ts
@@ -1,5 +1,4 @@
 import { Ring } from "./base-ring";
-import workletURL from './worklet-ring.ts?url';
 
 const canvasNode = document.querySelector<HTMLCanvasElement>('.canvas');
 const paintNode = document.querySelector<HTMLDivElement>('.paint');
@@ -31,7 +30,7 @@ const renderPaintNode = () => {
 	paintNode?.classList.remove('_hidden');
 
 	if (CSS.paintWorklet) {
-		CSS.paintWorklet.addModule(workletURL);
+		CSS.paintWorklet.addModule(new URL("worklet-ring.ts", import.meta.url));
 	}
 };
 
